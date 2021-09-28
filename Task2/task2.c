b9b055231ce9acf76154dc2aeecf3b05f29a95ab
Ilia Artemev <55094588+cucccumba@users.noreply.github.com>
2021-09-28 17:53:25 +0300

    Update task2.c

--- Task2/task2.c
+++ Task2/task2.c
@@ -1,3 +1,4 @@
+// со звездочкой
 #include <stdio.h>
 #include <stdlib.h>
 #include "omp.h"
@@ -5,10 +6,12 @@
 int main(int argc, char *argv[]) {
     int res = 0;
     int N = atoi(argv[1]);
+    // schedule(static) - чтобы каждому треду досталось поровну
     #pragma omp parallel for schedule(static) reduction(+:res)
     for(int i = 1; i <= N; ++i) {
         res = res + i;
-        printf("Thread #%d: sum = %d\n", omp_get_thread_num(), res);
+        // для отладки (печатает номер треда и результат его итерации)
+        //printf("Thread #%d: sum = %d\n", omp_get_thread_num(), res);
     }
     printf("Result: %d\n", res);
     return 0;
